19dcdcd5122db4617c447dd281ff87def0a19311
Marcos Duran <Marcos_Duran@ucne.edu.do>
2025-07-29 22:45:24 -0400

    Eliminando campos no utilizados en el MuscleGroupUiState.kt

--- app/src/main/java/edu/ucne/gymapp/presentation/musclegroup/MuscleGroupUiState.kt
+++ app/src/main/java/edu/ucne/gymapp/presentation/musclegroup/MuscleGroupUiState.kt
@@ -4,20 +4,11 @@ import edu.ucne.gymapp.data.local.entities.MuscleGroup
 import edu.ucne.gymapp.data.local.relation.MuscleGroupWithExercises
 
 data class MuscleGroupUiState(
-    val name: String = "",
-    val description: String = "",
-    val iconResource: String? = null,
     val muscleGroups: List<MuscleGroup> = emptyList(),
-    val orderedMuscleGroups: List<MuscleGroup> = emptyList(),
     val selectedMuscleGroup: MuscleGroup? = null,
     val selectedMuscleGroups: List<MuscleGroup> = emptyList(),
     val muscleGroupWithExercises: MuscleGroupWithExercises? = null,
-    val muscleGroupsWithExercises: List<MuscleGroupWithExercises> = emptyList(),
-    val multipleMuscleGroups: List<MuscleGroup> = emptyList(),
     val isLoading: Boolean = false,
     val errorMessage: String? = null,
-    val successMessage: String? = null,
-    val isCreated: Boolean = false,
-    val isUpdated: Boolean = false,
-    val isDeleted: Boolean = false
+    val successMessage: String? = null
 )
